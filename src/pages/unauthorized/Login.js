c8ac0caba219b528c3109dc12fda3a3c59403517
Pratama <pratamasetya99@gmail.com>
2017-12-03 07:17:19 +0700

    add page login

--- src/pages/unauthorized/Login.js
+++ src/pages/unauthorized/Login.js
@@ -2,12 +2,49 @@
 
 import React, { Component } from 'react';
 
-export default class Login extends Component {
+import { Form, Icon, Input, Button, Checkbox, Row, Col } from 'antd';
+const FormItem = Form.Item;
+
+class LoginForm extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
   render() {
+    const { getFieldDecorator } = this.props.form;
     return (
-      <div>
-        <h1>I'm the Login component</h1>
-      </div>
+      <Row>
+        <Col span={6} offset={9} style={{marginTop:150}}>
+          <Form onSubmit={this.handleSubmit} className="login-form">
+            <FormItem>
+              {getFieldDecorator('userName', {
+                rules: [{ required: true, message: 'Please input your username!' }],
+              })(
+                <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="Username" />
+              )}
+            </FormItem>
+            <FormItem>
+              {getFieldDecorator('password', {
+                rules: [{ required: true, message: 'Please input your Password!' }],
+              })(
+                <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="Password" />
+              )}
+            </FormItem>
+            <FormItem>
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                Log in
+              </Button>
+            </FormItem>
+          </Form>
+        </Col>
+      </Row>
     );
   }
 }
+
+const Login = Form.create()(LoginForm);
+export default Login

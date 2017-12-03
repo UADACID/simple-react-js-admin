2d21f67060af7bb6d20b9f0e0495f5fecd36eed7
Pratama <pratamasetya99@gmail.com>
2017-12-04 04:34:19 +0700

    add logout button

--- src/pages/authorized/Dashboard.js
+++ src/pages/authorized/Dashboard.js
@@ -1,12 +1,23 @@
 import React , { Component } from 'react'
-
+import { app as firebase } from '../../config/firebase'
 
 export default class Dashboard extends Component {
+
+  handleLogout = () => {
+    firebase.auth().signOut()
+    .then(()=>{
+      // Sign-out successful.
+    }).catch((error)=>{
+      console.log(error);
+      // An error happened.
+    });
+  }
 
   render(){
     return (
       <div>
         Dashboard
+        <button onClick={()=>this.handleLogout()}>logout</button>
       </div>
     )
   }
